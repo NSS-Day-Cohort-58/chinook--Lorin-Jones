8c0db98ba0436df4931f24c3b49a4f472d499c06
Lorin Jones <Lorin.Jones@gmail.com>
2022-11-29 11:35:36 -0500

    brazil cust invoice query

---
+++ brazil_customers_invoices.sql
@@ -0,0 +1,6 @@
+SELECT FirstName, LastName, InvoiceId, InvoiceDate, BillingCountry
+    FROM Customer
+        JOIN Invoice
+        ON Customer.CustomerId = Invoice.CustomerId
+        WHERE Country = "Brazil";
+ 
